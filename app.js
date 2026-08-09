f9b1589f7925400030c5caf617a48b24a8dace85
Aishwaryanerti <aishwaryanerti503@gmail.com>
2026-08-09 21:21:44 +0530

    Add Node.js project with tests and CI

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const add = require("./sum");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Node.js Server is Running");
+});
+
+app.get("/sum", (req, res) => {
+    const num1 = Number(req.query.num1);
+    const num2 = Number(req.query.num2);
+
+    const result = add(num1, num2);
+
+    res.json({
+        num1: num1,
+        num2: num2,
+        result: result
+    });
+});
+
+app.listen(3000, () => {
+    console.log(`Server running on port 3000`);
+});
