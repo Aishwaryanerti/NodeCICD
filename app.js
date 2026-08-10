0dd0d4ed6480bf38f68c8d5d3edb070ba5006923
Aishwaryanerti <aishwaryanerti503@gmail.com>
2026-08-10 11:55:41 +0530

    nodeCICD

--- app.js
+++ app.js
@@ -8,27 +8,3 @@ app.get("/hello", (req, res) => {
 });
  
 module.exports = app;
-const add = require("./sum");
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Node.js Server is Running");
-});
-
-app.get("/sum", (req, res) => {
-    const num1 = Number(req.query.num1);
-    const num2 = Number(req.query.num2);
-
-    const result = add(num1, num2);
-
-    res.json({
-        num1: num1,
-        num2: num2,
-        result: result
-    });
-});
-
-app.listen(3000, () => {
-    console.log(`Server running on port 3000`);
-});
